269fd54914dda0b9126808824d5a2116b766ed8c
NIRAV DIPAKKUMAR PATEL <101976669+NIRAVDEV@users.noreply.github.com>
2025-08-05 21:48:11 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import { NextAuthProvider } from './providers'
 import { Navbar } from '@/components/navbar'
+import { Analytics } from "@vercel/analytics/next"
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -21,6 +22,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <NextAuthProvider>
+          <Analytics/>
           <Navbar />
           <main>{children}</main>
         </NextAuthProvider>
