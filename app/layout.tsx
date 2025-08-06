cc70879bbc6585c425c5e93afc1b9193a752e893
NIRAV DIPAKKUMAR PATEL <101976669+NIRAVDEV@users.noreply.github.com>
2025-08-06 21:58:53 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import { Inter } from 'next/font/google'
 import { NextAuthProvider } from './providers'
 import { Navbar } from '@/components/navbar'
 import { Analytics } from "@vercel/analytics/next"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -23,6 +24,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <NextAuthProvider>
           <Analytics/>
+          <SpeedInsights/>
           <Navbar />
           <main>{children}</main>
         </NextAuthProvider>
